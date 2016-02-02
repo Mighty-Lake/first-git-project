5de8f39de386185aab6a3c86080bddfa53d57a95
Lenart Lipovsek <Lenart@Lenarts-MacBook-Pro.local>
2016-02-02 20:23:07 +0100

    Changed port for heroku compatibility

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 exports.start = function(){
     
     app.get('/', function(req, res){
         
-        res.send('Hello world');
+        res.send('Hello world by user 2');
     });
     
     app.listen(3000,function(){
